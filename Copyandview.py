d6ef46642a072628a9ed9c2e6a08c568ab99857a
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-07-01 19:46:20 +0530

    added copy and view

---
+++ Copyandview.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+arr[0] = 42    #change will happen because the copy has its own data
+
+print(arr)
+print(x)
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+arr[0] = 42
+
+print(arr)
+print(x)             #no change
